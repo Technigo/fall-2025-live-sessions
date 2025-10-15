54465303e3b2fde2ec5e171315eaffa5135aa563
Jennie Dalgren <jennie.dalgren@gmail.com>
2025-10-15 11:08:46 +0200

    set up typescript in week 8

---
+++ week-08/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('hello');
+let age;
+age = 11;
+let year;
+year = "2025";
+console.log(age);
+console.log(year);
---
+++ week-08/index.ts
@@ -0,0 +1,14 @@
+console.log('hello')
+
+let age: number
+
+age = 11
+
+let year: string
+
+year = "2025"
+
+console.log(age)
+console.log(year)
+
+
