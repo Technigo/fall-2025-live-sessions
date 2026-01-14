615f71f8a227d73e68ac95ce4dce30168224a363
Niclas Pahlfer <niclas@pahlfer.com>
2026-01-14 11:36:04 +0100

    feat: adds updated user store

--- week-18/zustand-practice/src/components/Users.jsx
+++ week-18/zustand-practice/src/components/Users.jsx
@@ -1,44 +1,60 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useUserStore } from "../stores/useUserStore";
 
 export const Users = () => {
-  const [newUser, setNewUser] = useState({ name: "", email: "" })
+  const users = useUserStore((state) => state.users);
+  const addUser = useUserStore((state) => state.addUser);
+  const removeUser = useUserStore((state) => state.removeUser);
+  const [newUser, setNewUser] = useState({ name: "", email: "" });
 
-  const users = [
-    { id: 1, name: "John Doe", email: "john@example.com" },
-    { id: 2, name: "Jane Smith", email: "jane@example.com" }
-  ]
+  // const users = [
+  //   { id: 1, name: "John Doe", email: "john@example.com" },
+  //   { id: 2, name: "Jane Smith", email: "jane@example.com" },
+  // ];
 
   const handleAddUser = () => {
-    //Here we will add the user to the store
-  }
+    addUser(newUser);
+  };
+
+  const handleRemoveUser = (userToRemove) => {
+    removeUser(userToRemove);
+  };
 
   return (
     <section className="dark">
       <h2>1. User Management App</h2>
       <p>a. Move the users array to the useUserStore and import it here</p>
-      <p>b. Make the handleAddUser work by creating a new action in the useUserStore</p>
-      <p>c. Make the remove user button work by creating a new action in the useUserStore</p>
+      <p>
+        b. Make the handleAddUser work by creating a new action in the
+        useUserStore
+      </p>
+      <p>
+        c. Make the remove user button work by creating a new action in the
+        useUserStore
+      </p>
       <input
         type="text"
         value={newUser.name}
-        onChange={e => setNewUser({ ...newUser, name: e.target.value })}
+        onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
         placeholder="Name"
       />
       <input
         type="email"
         value={newUser.email}
-        onChange={e => setNewUser({ ...newUser, email: e.target.value })}
+        onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
         placeholder="Email"
       />
       <button onClick={handleAddUser}>Add User</button>
       <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            <span>{user.name} ({user.email})</span>
-            <button>Remove user</button>
+        {users.map((user, index) => (
+          <li key={index}>
+            <span>
+              {user.name} ({user.email})
+            </span>
+            <button onClick={() => handleRemoveUser(user)}>Remove user</button>
           </li>
         ))}
       </ul>
     </section>
-  )
-}
+  );
+};
--- week-18/zustand-practice/src/stores/useUserStore.jsx
+++ week-18/zustand-practice/src/stores/useUserStore.jsx
@@ -1,6 +1,38 @@
-import { create } from "zustand"
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-export const useUserStore = create(() => ({
-  // Here we will store the users
-  users: []
-}))
+// const initialUserState = [
+//   { id: 1, name: "John Doe", email: "john@example.com" },
+//   { id: 2, name: "Jane Smith", email: "jane@example.com" },
+//   // { id: 3, name: "Jane Smith", email: "jane@example.com" },
+// ];
+
+export const useUserStore = create(
+  devtools((set, get) => ({
+    users: [
+      { id: 1, name: "John Doe", email: "john@example.com" },
+      { id: 2, name: "Jane Smith", email: "jane@example.com" },
+    ],
+    addUser: (newUser) => {
+      const usersListLength = get().users.length;
+
+      const userToAdd = {
+        ...newUser,
+        id: usersListLength + 1,
+      };
+
+      return set((state) => ({
+        users: [...state.users, userToAdd],
+      }));
+    },
+    removeUser: (userToRemove) => {
+      return set((state) => {
+        const newUsers = state.users.filter((u) => u.id !== userToRemove.id);
+
+        return {
+          users: newUsers,
+        };
+      });
+    },
+  })),
+);
